3a6176edaed4e7b779d7494b2e5c9b84542bdff1
Malik Abid <malikabidzz0306@gmail.com>
2023-08-11 12:12:36 +0500

    class6

---
+++ class pr/class6.js
@@ -0,0 +1,24 @@
+// write ts program that's calculate sum of two number using function without parameter
+function sum() {
+    var num1 = 30;
+    var num2 = 130;
+    var sum = num1 + num2;
+    console.log("Sum Result is " + sum);
+}
+sum();
+// write ts program that's calculate sum of two numbers, 40 or 90 using function without parameter
+function sum2() {
+    var num1 = 40;
+    var num2 = 90;
+    var sum = num1 + num2;
+    console.log("Sum2 Result is " + sum);
+}
+sum2();
+// write ts program that's calculate sum of two number using function with parameter
+function sum3(num1, num2) {
+    var sum = num1 + num2;
+    console.log("Sum3 Result is " + sum);
+}
+sum3(30, 130);
+// write ts program that's calculate sum of two numbers, 40 or 90 using function with parameter
+sum3(40, 90);
