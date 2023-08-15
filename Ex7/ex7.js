7cbbd9f89b4d5eaca5cbe7f4ad57a41789d49424
Malik Abid <malikabidzz0306@gmail.com>
2023-08-15 18:58:41 +0500

    update

---
+++ Ex7/ex7.js
@@ -0,0 +1,6 @@
+// Type inferences in typescript
+//typescript automatically check variable types
+var num = 10; //variable type is number
+var nam = "Abid"; //variable type is String
+var signIn = true; //variable type is bool
+var data; // variable type is any
---
+++ Ex7/ex7.ts
@@ -0,0 +1,9 @@
+// Type inferences in typescript
+
+//typescript automatically check variable type is called inferences
+
+let num = 10; //variable type is number
+let nam = "Abid"; //variable type is String
+let signIn = true; //variable type is bool
+let data; // variable type is any
+
