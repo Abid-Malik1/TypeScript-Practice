9f1dad25db450381cfdd68a1ef4a6690786d3165
Malik Abid <malikabidzz0306@gmail.com>
2023-08-30 15:19:08 +0500

    update

---
+++ TypeScript voidF/voidf.js
@@ -0,0 +1,7 @@
+var x = 30;
+var y = 40;
+var today;
+function printData() {
+    return today = new Date;
+}
+console.log(printData());
---
+++ TypeScript voidF/voidf.ts
@@ -0,0 +1,10 @@
+let x = 30;
+let y = 40;
+let today;
+
+function printData() {
+    return today = new Date;
+}
+
+
+console.log(printData())
